6d62544d9fbf609b2e3772c9c054e0b346cb5abd
Glory <gauthier.lory@protonmail.com>
2020-03-29 17:11:21 +0200

    edit page with data on bdd

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article.js')
 const articleRouter = require('./routes/article.js')
 const app = express()
 
+mongoose.connect('mongodb://localhost:27017/blog', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
 app.set('view engine', 'ejs')
 
-app.use('/articles',articleRouter)
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/',(req, res) => {
-    const articles = [{
-        title: 'Test articles',
-        createdAt: new Date(),
-        description: 'Test description'
-
-    },
-    {
-        title: 'Test articles 2',
-        createdAt: new Date,
-        description: 'Test description 2'
-
-    }]
-    res.render('index', { articles : articles })
+app.get('/',async (req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    })
+    res.render('articles/index', { articles : articles })
 })
+
+
+app.use('/articles',articleRouter)
 
 app.listen(5000)
