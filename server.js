f803da02467849a3f36f4416db3512b77794d45a
Glory <gauthier.lory@protonmail.com>
2020-03-30 19:16:17 +0200

    user register on bdd

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article.js')
+const User = require('./models/user.js')
 const articleRouter = require('./routes/article.js')
 const methodOverride = require('method-override')
+const bcrypt = require('bcrypt')
 const app = express()
 
 mongoose.connect('mongodb://localhost:27017/blog', { 
@@ -20,6 +22,25 @@ app.get('/',async (req, res) => {
         createdAt: 'desc'
     })
     res.render('articles/index', { articles : articles })
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs', { register: 'register page'})
+})
+
+app.post('/register', async (req, res) =>{
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    })
+    try{
+        user = await user.save()
+        res.redirect('/login')
+    }catch{
+        res.redirect('/register')
+    }
 })
 
 
