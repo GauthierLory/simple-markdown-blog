c0df6e91a1bd70173b94c8b96b225dc9ded20e06
Glory <gauthier.lory@protonmail.com>
2020-12-30 17:41:57 +0100

    user list view

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
 const bcrypt = require('bcrypt')
 const passport = require('passport')
+const Users = require('../models/user')
 const express = require('express')
 const router = express.Router()
 
 const User = require('../models/user.js')
 const {isAuthenticated, isNotAuthenticated} = require('../config/credential')
 
-router.get('/info', (req, res) => {
-    res.render('auth/user.ejs', { name: req.user.name })
+router.get('/info',isAuthenticated, async(req, res) => {
+    const users = await User.find()
+    res.render('user/index.ejs', { users: users })
 })
 module.exports = router;
